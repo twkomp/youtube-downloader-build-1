8bf1789534ae938e0421e8921cca04e2c5514b1a
twkomp <twkomp@gmail.com>
2017-07-26 23:33:19 -0400

    v2 with optional downloads folder

---
+++ v2/downloader.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+import youtube_dl, os
+import shutil
+import pygtk
+movefiletodownloads = True
+pygtk.require('2.0')
+import gtk
+os.chdir(".//")
+ydl_opts = {}
+clip = gtk.clipboard_get()
+text = clip.wait_for_text()
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([text])
+dira = os.listdir(".")
+if movefiletodownloads == True:
+  if os.path.isdir("./downloads") == True:
+    for item in dira:
+      if item == 'downloader.py':
+        pass
+      else:
+        dirb = item
+    try:
+      shutil.move(dirb, "./downloads")
+    except shutil.Error:
+      print "allready exists, deleting"
